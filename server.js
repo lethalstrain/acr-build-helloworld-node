f1e2772f3bbeb49672259c70b02cc649ec1b5f7e
lethalstrain <wouter.isdabom@gmail.com>
2020-09-15 13:57:42 +0200

    testing ACR task on commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 80
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
+  response.write("<style>background-image:url('https://media1.tenor.com/images/ba044b56a5d6365b0deed758f1265998/tenor.gif?itemid=15356887')</style>")
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
